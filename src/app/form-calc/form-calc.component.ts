b515303b68d19ae98bbbe816370e983c1b261425
luana.arruda <luanamarrudaa@gmail.com>
2024-03-05 11:07:03 -0300

    Add mult

--- src/app/form-calc/form-calc.component.ts
+++ src/app/form-calc/form-calc.component.ts
@@ -9,16 +9,19 @@ export class FormCalcComponent {
   num1: number = 0;
   num2: number = 0;
   sum: number = 0;
-  subtract: number = 0;
+  sub: number = 0;
   mult: number = 0;
 
   onClickSum(){
     this.sum = this.num1 + this.num2;
   }
 
-  onClickSubtract(){
+  onClickSub(){
     this.sum = this.num1 - this.num2;
   }
 
+  onClickMult(){
+    this.sum = this.num1 * this.num2;
+  }
 
 }
