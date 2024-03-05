e3384dc16fb1646784d079bbca713c977368ade5
luana.arruda <luanamarrudaa@gmail.com>
2024-03-05 13:08:08 -0300

    Add div and resp

--- src/app/form-calc/form-calc.component.ts
+++ src/app/form-calc/form-calc.component.ts
@@ -8,20 +8,22 @@ import { Component } from '@angular/core';
 export class FormCalcComponent {
   num1: number = 0;
   num2: number = 0;
-  sum: number = 0;
-  sub: number = 0;
-  mult: number = 0;
+  resp: number = 0;
+
 
   onClickSum(){
-    this.sum = this.num1 + this.num2;
+    this.resp = this.num1 + this.num2;
   }
 
   onClickSub(){
-    this.sum = this.num1 - this.num2;
+    this.resp = this.num1 - this.num2;
   }
 
   onClickMult(){
-    this.sum = this.num1 * this.num2;
+    this.resp = this.num1 * this.num2;
   }
 
+  onClickDiv(){
+    this.resp = this.num1 / this.num2;
+  }
 }
