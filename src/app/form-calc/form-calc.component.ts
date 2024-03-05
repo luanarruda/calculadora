f99b8b3a9937fbf42e1c8ec2a9a6a3bea5373985
luana.arruda <luanamarrudaa@gmail.com>
2024-03-05 10:58:54 -0300

    Add to subtract

--- src/app/form-calc/form-calc.component.ts
+++ src/app/form-calc/form-calc.component.ts
@@ -9,9 +9,16 @@ export class FormCalcComponent {
   num1: number = 0;
   num2: number = 0;
   sum: number = 0;
+  subtract: number = 0;
+  mult: number = 0;
 
   onClickSum(){
     this.sum = this.num1 + this.num2;
   }
+
+  onClickSubtract(){
+    this.sum = this.num1 - this.num2;
+  }
+
 
 }
